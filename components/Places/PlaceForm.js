2071921001073d07cec4df717950f0033ebc0c85
chaikovskyit <chaikovskyit@gmail.com>
2023-01-16 16:29:19 +0200

    added stack navigation

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const PlaceForm = () => {
+  return (
+    <View>
+      <Text>The place form</Text>
+    </View>
+  );
+};
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({});
--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
+import PlaceForm from "../components/Places/PlaceForm";
 
 const AddPlace = () => {
   return (
     <View>
       <Text>Add Place</Text>
+      <PlaceForm />
     </View>
   );
 };
